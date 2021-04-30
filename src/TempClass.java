13086ea80540931511c50c29f3b5d9c3fef834ea
Shreyas <shreyaspacharne02@gmail.com>
2021-04-30 13:53:21 +0530

    changes - message and date time

--- src/TempClass.java
+++ src/TempClass.java
@@ -1,8 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TempClass
 {
     public static void main(String args[])  //static method
     {
-        System.out.println("Main method");
+        System.out.println("Hello World");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
     }
 }
 
