c0e2dbae6fa08901226e98249f7b6fdb4a52bedf
Shreyas <shreyaspacharne02@gmail.com>
2021-05-03 17:14:23 +0530

    ;eap year

---
+++ src/leapyear.java
@@ -0,0 +1,15 @@
+class leapyear
+{
+    public static void main(String arg[])
+    {
+        int year=2000;
+        if (((year%4==0)&&(year%100!=0))||(year%400==0))
+        {
+           System.out.println("This year is leaf year");
+        }
+        else
+        {
+            System.out.println("This year is not leaf year");
+        }
+    }
+}
