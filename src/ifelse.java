a525e0c5468768ef82d5ab31f786dcce0753d6bb
Shreyas <shreyaspacharne02@gmail.com>
2021-05-03 14:48:19 +0530

    if else

---
+++ src/ifelse.java
@@ -0,0 +1,16 @@
+class ifelse
+{
+    public static void main(String args[])
+
+    {
+        int age=20;
+        if (age>18)
+        {
+            System.out.println("he is greater than 18");
+        }
+        else
+        {
+            System.out.println("Not allowed");
+        }
+    }
+}
