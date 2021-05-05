06df399d0e6e372fba5bd4059e2887d07d19332c
Shreyas <shreyaspacharne02@gmail.com>
2021-05-05 14:31:08 +0530

    pyramid ascending and descending

---
+++ src/pyramid.java
@@ -0,0 +1,28 @@
+class pyramid
+/*
+{
+    public static void main(String[] args)
+    {
+        for (int i=1;i<=4;i++)
+        {
+            for (int j=1;j<=i;j++)
+                System.out.print("@ ");
+            System.out.println();
+        }
+
+    }
+}*/
+ {
+    public static void main(String[] args)
+    {
+        int num=6;
+        for(int i=1;i<=num;i++)
+        {
+            for(int j=num;j>=i;j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
