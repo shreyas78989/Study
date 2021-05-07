36232a685abd2c0c9080cdf9d96a9e3032d63654
Shreyas <shreyaspacharne02@gmail.com>
2021-05-07 15:09:25 +0530

    infinite loop

---
+++ src/infinativewhile.java
@@ -0,0 +1,11 @@
+class infinativewhile
+{
+    public static void main(String[] args)
+    {
+        while (true)
+        {
+            System.out.println("Infinative loop");
+           // break;
+       }
+    }
+}
