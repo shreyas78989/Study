ce47e77222aa466e383d2c9780f1db50ebb0e306
Shreyas <shreyaspacharne02@gmail.com>
2021-05-07 16:39:22 +0530

    Infinit loop in do while

---
+++ src/infinitdowhile.java
@@ -0,0 +1,11 @@
+class infinitedowhile
+{
+    public static void main(String[] args)
+    {
+        do
+        {
+            System.out.println("Infinite loop ");
+        }
+        while (true);
+    }
+}
